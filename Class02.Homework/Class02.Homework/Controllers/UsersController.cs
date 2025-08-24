b651f93c81acb3efbf2b4f85029060f5bece8771
s1ckys <131174539+s1ckys@users.noreply.github.com>
2025-08-24 15:59:07 +0200

    Add files via upload

---
+++ Class02.Homework/Class02.Homework/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Class02.Homework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAll()
+        {
+            return Ok(StaticDb.Users);
+        }
+        [HttpGet("{index}")]
+        public ActionResult<string> GetUser(int index)
+        {
+            if(index == null)
+            {
+                return BadRequest("User index cannot be null");
+            }
+            if(index < 0)
+            {
+                return BadRequest("User index cannot be negative number");
+            }
+            if(index >= StaticDb.Users.Count)
+            {
+                return NotFound($"There is no user with index {index}");
+            }
+            return Ok(StaticDb.Users[index]);
+        }
+    }
+
+}
---
+++ Class02.Homework/Class02.Homework/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace Class02.Homework
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>()
+        {
+            "User1",
+            "User2",
+            "User3"
+        };
+
+        
+    }
+}
